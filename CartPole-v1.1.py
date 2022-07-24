4c1153ee151d211e0982f8e80c44b368b837679a
kkimlee <40078267+kkimlee@users.noreply.github.com>
2022-07-24 15:20:06 +0900

    Create CartPole-v1.1.py
    CartPole 예제2

---
+++ CartPole-v1.1.py
@@ -0,0 +1,30 @@
+import gym
+
+#%% 환경 생성 및 초기화
+env = gym.make("CartPole-v1")
+
+#%% 시작 전 환경 재설정
+env.reset()
+
+#%% 게임 실행
+for i in range(10):
+    # 변수 초기화
+    done = False # 종료 변수
+    game_rew = 0 # 보상
+    
+    while not done:
+        # 랜덤 액션 선택
+        action = env.action_space.sample()
+        # 환경 내 한 개의 스텝
+        obs, rew, done, info = env.step(action)
+        # 보상 계산
+        game_rew += rew
+        # 게임 렌더링
+        env.render()
+        
+        if done:
+            print(f"Episode {i} finished, reward:{game_rew}")
+            env.reset()
+
+#%% 환경 종료
+env.close()
